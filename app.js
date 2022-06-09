fa94cefb5ad8330eaf267e4c1c7e8cab6499eba1
hanifulIhsan <107019649+hanifulIhsan@users.noreply.github.com>
2022-06-09 16:01:39 +0700

    asdasd

---
+++ app.js
@@ -0,0 +1,30 @@
+//Imports
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+
+const app= express()
+const port = 3000
+
+//Static File
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/images', express.static(__dirname + 'public/images'))
+
+
+//set template engine
+app.use(expressLayouts)
+app.set('layout', './layouts/main-layout.ejs')
+app.set('view engine', 'ejs')
+
+//Navigation
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.get('/makanan', (req, res) => {
+    res.render('makanan')
+})
+
+
+//Listen on Port 3000
+app.listen(port, () => console.log(`app listening on http://localhost:${port}`))
